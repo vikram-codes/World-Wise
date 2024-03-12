1c6f7ab84fae35c799e87dd61655bcda61864119
Vikram Saini <vikramjit.personal@gmail.com>
2024-03-12 13:30:03 +0530

    Implemented BrowserRouter

--- World-Wise/src/App.jsx
+++ World-Wise/src/App.jsx
@@ -1,12 +1,23 @@
 import Homepage from './pages/Homepage'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
 
   return (
-    <>
-      <Homepage />
-    </>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Homepage />} />
+      <Route path='product' element={<Product />} />
+      <Route path='pricing' element={<Pricing />} />
+      <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+      
+    
   )
 }
 
