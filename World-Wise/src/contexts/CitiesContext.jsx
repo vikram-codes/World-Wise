22a3d11d11e8884f2a981cf0b3be28e691f184fb
Vikram Saini <vikramjit.personal@gmail.com>
2024-03-21 15:46:04 +0530

    Fixed eslint unnecessary errors

--- World-Wise/src/contexts/CitiesContext.jsx
+++ World-Wise/src/contexts/CitiesContext.jsx
@@ -1,34 +1,30 @@
 /* eslint-disable no-unused-vars */
-import { createContext, useEffect, useState } from "react"
+import { createContext, useEffect, useState } from "react";
 
 const CitiesContext = createContext();
 
-function CitiesProvider() {
-    const BASE_URL = 'http://localhost:8000'
+function CitiesProvider({ children }) {
+  const BASE_URL = "http://localhost:8000";
 
-  const [cities, setCities] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function() {
+  useEffect(function () {
     async function fetchCities() {
       try {
-    setIsLoading(true)
-    const response = await fetch(`${BASE_URL}/cities`)
-       const data = await response.json()
-    setCities(data) }
-    catch (error) {
-      alert('There was an error loading data...')
-    } finally {
-      setIsLoading(false)
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+      } catch (error) {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
     }
-  }
-    fetchCities()
-}, [])
-    return (
-        <div>
-            
-        </div>
-    )
+    fetchCities();
+  }, []);
+  return <div></div>;
 }
 
-export default CitiesProvider
+export default CitiesProvider;
