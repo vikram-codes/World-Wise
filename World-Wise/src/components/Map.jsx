6dbe481301350051bc62ccbbdd11c22d4ffc8c1f
Vikram Saini <vikramjit.personal@gmail.com>
2024-03-22 17:43:52 +0530

    Installed React Leaflet Library

--- World-Wise/src/components/Map.jsx
+++ World-Wise/src/components/Map.jsx
@@ -1,28 +1,17 @@
 /* eslint-disable no-unused-vars */
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import styles from './Map.module.css'
-
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 
 function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const navigate = useNavigate();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
-
-    return (
-        <div className={styles.mapContainer} onClick={()=>navigate('form')} >
-            <h1>Map
-            position: 
-            {lat}
-            {lng}
-            </h1>
-            <button onClick={()=> {setSearchParams({lat: 23, lng: 50})}} >
-                Change Position
-            </button>
-        </div>
-    )
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}></div>
+  );
 }
 
-export default Map
+export default Map;
