6c63f9ef98c7282d5a8469704c9e9c1b1dad0c56
Vikram Saini <vikramjit.personal@gmail.com>
2024-03-22 18:08:51 +0530

    Installed React Leaflet Library

--- World-Wise/src/components/Map.jsx
+++ World-Wise/src/components/Map.jsx
@@ -1,16 +1,36 @@
 /* eslint-disable no-unused-vars */
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapContainer, setMapContainer] = useState([40, 0]);
   const navigate = useNavigate();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}></div>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapContainer}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapContainer}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
