2dbce2230b8fe02cee4f3b90b2f0a6c1c71e941b
Vikram Saini <vikramjit.personal@gmail.com>
2024-03-29 18:40:43 +0530

    Implemented login feature

--- World-Wise/src/pages/Login.jsx
+++ World-Wise/src/pages/Login.jsx
@@ -1,48 +1,57 @@
 import styles from "./Login.module.css";
-import {Link} from 'react-router-dom'
-import {useState} from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
 import Button from "../components/Button";
-
+import { useAuth } from "../contexts/FakeAuthContext";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+    if (!email || !password) alert("Please enter your details");
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/app", { replace: true });
+  }, [isAuthenticated, navigate]);
 
   return (
     <div>
-    <main className={styles.login}>
-      <PageNav />
-      <form className={styles.form}>
-        <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </div>
+      <main className={styles.login}>
+        <PageNav />
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.row}>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
 
-        <div className={styles.row}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </div>
+          <div className={styles.row}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </div>
 
-        <div>
-          <Link to='/app' >
-          <Button type="primary">Login</Button>
-          </Link>
-        </div>
-      </form>
-    </main>
+          <div>
+            <Button type="primary">Login</Button>
+          </div>
+        </form>
+      </main>
     </div>
-
   );
 }
