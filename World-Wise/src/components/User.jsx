f1bf2c8eae57aaef7c400fc5f27cbd03a4038c19
Vikram Saini <vikramjit.personal@gmail.com>
2024-03-29 19:11:40 +0530

    Fixed the bug which was not reading the user details

--- World-Wise/src/components/User.jsx
+++ World-Wise/src/components/User.jsx
@@ -1,16 +1,22 @@
+import { useEffect } from "react";
 import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./User.module.css";
+import { useNavigate } from "react-router-dom";
 
 function User() {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
-  function handleClick() {}
+  // useEffect(() => {
+  //   logout();
+  //   navigate("/app");
+  // }, [logout, navigate]);
 
   return (
     <div className={styles.user}>
       <img src={user.avatar} alt={user.name} />
       <span>Welcome, {user.name}</span>
-      <button onClick={handleClick}>Logout</button>
+      <button>Logout</button>
     </div>
   );
 }
--- World-Wise/src/contexts/FakeAuthContext.jsx
+++ World-Wise/src/contexts/FakeAuthContext.jsx
@@ -2,6 +2,13 @@ import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 const initialState = {
   user: null,
   isAuthenticated: false,
@@ -12,7 +19,7 @@ function reducer(state, action) {
     case "login":
       return {
         ...state,
-        user: "jack",
+        user: action.payload,
         isAuthenticated: true,
       };
     case "logout":
@@ -26,13 +33,6 @@ function reducer(state, action) {
   }
 }
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
@@ -41,7 +41,7 @@ function AuthProvider({ children }) {
 
   function login(email, password) {
     if (email === FAKE_USER.email && password === FAKE_USER.password)
-      dispatch({ type: "login" });
+      dispatch({ type: "login", payload: FAKE_USER });
   }
   function logout() {
     dispatch({ type: "logout" });
